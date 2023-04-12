18106c817b79e52e4af73c80bcd7c42e030781d0
sunshanqian <sunshanqian@outlook.com>
2023-04-12 15:24:56 +0900

    Update main.py
    202304121522

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 # from pymouse import PyMouse
-from pykeyboard import PyKeyboard
+from pynput.keyboard import Key, Controller
 
 import time
 
-keyboard = PyKeyboard()
+keyboard = Controller()
 
 for i in range(5, 0, -1):
     print("[INFO] Count down: " + str(i))
     time.sleep(1)
 
 print("[INFO] Playing...")
-keyboard.tap_key('a', n=5, interval=2)
+
+keyboard.press('a')
+
+time.sleep(5)
+
+keyboard.release('a')
+
+with keyboard.pressed(Key.shift, 's'):          # can't work without with
+    pass
+
+# keyboard.pressed(Key.shift, 's')
+
+time.sleep(5)
+
+with keyboard.pressed('a'):
+    pass
+
+# keyboard.pressed('a')
+
 
