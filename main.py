f6163edad0fb16b5af3606034ea5c20a676fe786
sunshanqian <sunshanqian@outlook.com>
2023-04-13 23:31:54 +0900

    Update main.py
    202304132331

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ def playing():
     # function like print or sleep can deal
     # this problem but pass can not
     #############################################
-    file_object = open("twinklestar.ms", 'r')
+    file_object = open("lemon.ms", 'r')
     try:
         text = file_object.read()
     finally:
